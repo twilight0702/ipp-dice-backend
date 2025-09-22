b36d56ad2bafb1313a8ca614a6fb26b519e966aa
Twilight <2475911580@qq.com>
2025-09-22 13:13:14 +0800

    init

---
+++ src/main/java/com/ippclub/ippdicebackend/IppDiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ippclub.ippdicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IppDiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IppDiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ippclub/ippdicebackend/IppDiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ippclub.ippdicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IppDiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
